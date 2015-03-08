f183e8ac67a620639a211a86bec6e242be1fdd03
ChristopherSalam <christopher.salam@gmail.com>
2015-03-08 04:07:31 -0700

    grayscale, about fix, directory, redundancy

--- js/data/about.js
+++ js/data/about.js
@@ -1,38 +1,21 @@
 App.ABOUT = [
 {
+image: 'img/about/Chris.jpg',
 person: 'Chris',
 blurb: 'I started collecting bikes to make sure I always had one.',
-story: 'When my brand new-to-me Raleigh Pursuit bicycle was stolen during finals week, a bicycle purchased to replace another stolen bicycle, I felt prompted to set myself against theft, and I proceeded to buy a fleet of bicycles, craiglisting all around rural Inland California. I loved the stories of purchasing and talking to buyers more than fixing and reselling. These bicycles, placed outside my house (a <a href="http://bagginsend.net/history.html"> dome </a> at the time), caused many of my neighbors to ask about their prices and availability. Reluctant initially, I saw that what once was backup for me was leading to a set of successful transactions, vintage bicycles moving on to new owners who would create stories of their own. Fast forward to present day, bicycle knowledge, purchasing and advice, and history are chief interests of mine, as well as occasional consignments.'
+story: 'After my first bike theft, I felt prompted to set myself and I proceeded to buy a fleet of bicycles, craiglisting all around rural Inland California. My fleet parked out front caused many of my neighbors to ask about their prices and availability. Reluctant initially, I got into it after a few tranasctions. Fast forward to present day, bicycle knowledge, purchasing and advice, and history are chief interests of mine, as well as occasional consignments.'
 },
 {
+image: 'img/about/Darach.jpg',
 person: 'Darach',
-blurb: 'I\'m a dude',
-story: 'My story'
+blurb: 'I\'ve got a big heart and a big gap in my teeth',
+story: 'I\'m a bioinformatics & microbiology researcher with a macro-level interest in Bicycles. Hobbies include getting hit by vehicles and getting hit by polo sticks on tennis courts.'
 },
 {
+image: 'img/about/Dan.jpg',
 person: 'Dan',
-blurb: 'I\'m a dude',
-story: 'My story'
-},
-{
-person: 'Carlos',
-blurb: 'I\'m a dude',
-story: 'My story'
-},
-{
-person: 'Drew',
-blurb: 'I\'m a dude',
-story: 'My story',
-},
-{
-person: 'Jeremy and Scott',
-blurb: 'I\'m father and son dudes',
-story: 'Our story'
-},
-{
-person: 'Eric',
-blurb: 'I\'m a dude',
-story: 'My story'
+blurb: 'I\'m a builder of bikes, violins, and epic road trips!',
+story: 'I\'ve been buying and selling bicycles and other artifacts out the central valley. I keep relatively few bicycles of my own, but I do keep violins and recumbants.'
 }
 ];
 
