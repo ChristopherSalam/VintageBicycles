cce5e6dd02bd2e30468362b87873d4f108846d14
ChristopherSalam <christopher.salam@gmail.com>
2015-03-22 11:55:18 -0700

    links work! *bows*

--- js/data/references.js
+++ js/data/references.js
@@ -4,203 +4,203 @@ make: "General",
 id:1,
 intro: "Bicycle references cover the net. This site will break each of these sites into categories to make them easier to navigate.",
 origin: "earth",
-desc: "Sheldon Brown is the original bicycle reference writer online, and is a inspiration to many. I first discovered Sheldon Brown in college, and read through his site. I personally loved his personal bikes the most, as they told me more about Sheldon as a person. This site hopes to someday give users the power to build their own page like the late great S.B. and then to look through everyone else\'s works as well. Check out sheldon\'s site!",
-href: 'http://sheldonbrown.com'
+text: "Sheldon Brown is the original bicycle reference writer online, and is a inspiration to many. I first discovered Sheldon Brown in college, and read through his site. I personally loved his personal bikes the most, as they told me more about Sheldon as a person. This site hopes to someday give users the power to build their own page like the late great S.B. and then to look through everyone else\'s works as well.",
+url: 'http://sheldonbrown.com'
 },
 {
 make:"British Bicycles",
 id:2,
 origin: "U.K.",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Japanese Bicycles",
 id:3,
 origin: "Japan",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"American Bicycles",
 id:4,
 origin: "U.S.A.",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Italian Bicycles",
 id:5,
 origin: "Italian",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"French Bicycles",
 id:6,
 origin: "French",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"European Bicycles",
 id:7,
 origin: "European",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Bianchi",
 id:8,
 origin: "Italian",
-desc: 'Offical Site',
-href: 'http://www.bianchiusa.com/'
+text: 'Offical Site',
+url: 'http://www.bianchiusa.com/'
 },
 {
 make:"Bridgestone",
 id:9,
 origin: "Japan",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Cannondale",
 id:10,
 origin: "U.S.A.",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Centurion",
 id:11,
 origin: "Japan",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Fuji",
 id:12,
 origin: "Japan",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Gitane",
 id:13,
 origin: "French",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Miyata",
 id:14,
 origin: "Japan",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Nishiki",
 id:15,
 origin: "Japan",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Novara",
 id:16,
 origin: "U.S.A.",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Olmo",
 id:17,
 origin: "Italian",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Panasonic",
 id:18,
 origin: "Japan",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Peugeot",
 id:19,
 origin: "French",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Raleigh",
 id:20,
 origin: "U.K.",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Schwinn",
 id:21,
 origin: "U.S.A.",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Specialized",
 id:22,
 origin: "U.S.A.",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Terry",
 id:23,
 origin: "Japan",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Trek",
 id:24,
 origin: "U.S.A.",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"American Department Store",
 id:25,
 origin: "U.S.A.",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"Japanese Department Store",
 id:26,
 origin: "Japan",
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make:"You tell me",
 id:27,
 origin: null,
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make: "Fixed Gear Conversions",
 id:28,
 origin: null,
-desc: [],
-href: []
+text: [],
+url: null
 },
 {
 make: "Circus Bikes & Special Projects",
 id:29,
 origin: null,
-desc: [],
-href: []
+text: [],
+url: null
 }
 ]
