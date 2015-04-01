eeb515d57b729aa6890a515c709757580377c81e
chrissalam <christopher.salam@gmail.com>
2015-03-31 18:09:35 -0700

    added 4 types and collapse for makes
    French, Specialized, Centurion, and Nishiki controllers, routes, and
    indices were added. I also found a way to add a collapse bar for makes,
    however, it needs to be corrected to default closed when in a smaller
    window.

--- js/ember/controllers.js
+++ js/ember/controllers.js
@@ -14,7 +14,7 @@ App.ReferencesController = Em.ArrayController.extend({
 
 App.CatalogController = Em.ArrayController.extend({
 	//sortProperties: ['make','model']
-	catalogCount: Em.computed.alias('length')
+	catalogCount: Em.computed.alias('length'),
 });
 
 App.BianchiController = Em.ArrayController.extend({
@@ -27,9 +27,29 @@ App.CannondaleController = Em.ArrayController.extend({
 	cannondaleCount: Em.computed.alias('sortCannondale.length')
 });
 
+App.CenturionController = Em.ArrayController.extend({
+	sortCenturion: Em.computed.filterBy('model', 'make', 'Centurion'), 
+	centurionCount: Em.computed.alias('sortCenturion.length')
+});
+
+App.NishikiController = Em.ArrayController.extend({
+	sortNishiki: Em.computed.filterBy('model', 'make', 'Nishiki'), 
+	nishikiCount: Em.computed.alias('sortNishiki.length')
+});
+
+App.SpecializedController = Em.ArrayController.extend({
+	sortSpecialized: Em.computed.filterBy('model', 'make', 'Specialized'), 
+	specializedCount: Em.computed.alias('sortSpecialized.length')
+});
+
 App.PeugeotController = Em.ArrayController.extend({
 	sortPeugeot: Em.computed.filterBy('model', 'make', 'Peugeot'), 
 	peugeotCount: Em.computed.alias('sortPeugeot.length')
+});
+
+App.RaleighController = Em.ArrayController.extend({
+	sortRaleigh: Em.computed.filterBy('model', 'make', 'Raleigh'), 
+	raleighCount: Em.computed.alias('sortRaleigh.length')
 });
 
 App.TrekController = Em.ArrayController.extend({
@@ -40,6 +60,11 @@ App.TrekController = Em.ArrayController.extend({
 App.JapaneseController = Em.ArrayController.extend({
 	sortJapanese: Em.computed.filterBy('model', 'country', 'japan'), 
 	japaneseCount: Em.computed.alias('sortJapanese.length')
+});
+
+App.FrenchController = Em.ArrayController.extend({
+	sortFrench: Em.computed.filterBy('model', 'country', 'france'), 
+	frenchCount: Em.computed.alias('sortFrench.length')
 });
 
 App.EnglishController = Em.ArrayController.extend({
--- js/ember/router.js
+++ js/ember/router.js
@@ -3,13 +3,19 @@ App.Router.map(function() {
   	this.resource('person', {path: '/:person' });
   });
   this.resource('catalog', function() {
+    this.resource('make', {path: '/:make'});
   });
   this.resource('bianchi', {path: '/catalog/bianchi/:bianchi'});  
   this.resource('cannondale', {path: '/catalog/cannondale/:cannondale'});
+  this.resource('centurion', {path: '/catalog/centurion/:centurion'});
+  this.resource('nishiki', {path: '/catalog/nishiki/:nishiki'});
   this.resource('peugeot', {path: '/catalog/peugeot/:peugeot'});
+  this.resource('raleigh', {path: '/catalog/raleigh/:raleigh'});
+  this.resource('specialized', {path: '/catalog/specialized/:specialized'}); 
   this.resource('trek', {path: '/catalog/trek/:trek'});
   this.resource('japanese', {path: '/catalog/japanese/:japanese'})
   this.resource('english', {path: '/catalog/english/:english'})
+  this.resource('french', {path: '/catalog/french/:french'})
   this.resource('italian', {path: '/catalog/italian/:italian'})
   this.resource('usa', {path: '/catalog/usa/:usa'})
   this.route('bike', {path: '/catalog/:make/:model'});
