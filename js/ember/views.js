fb4e2f1b78f7eed1124352f418752437bf16cb5b
ChristopherSalam <christopher.salam@gmail.com>
2015-05-13 16:27:06 -0700

    resolved errors

---
+++ js/ember/views.js
@@ -0,0 +1,179 @@
+App.IndexView = Em.View.extend({
+    templateName: 'index',
+    didInsertElement: function() {
+        $('body').addClass('indexView')
+    },
+    willClearRender: function() {
+        $('body').removeClass('indexView')
+    }
+})
+
+App.ReferencesView = Em.View.extend({
+    templateName: 'references',
+    didInsertElement: function() {
+        $('body').addClass('referencesView')
+    },
+    willClearRender: function() {
+        $('body').removeClass('referencesView')
+    }
+});
+
+App.AboutView = Em.View.extend({
+    templateName: 'about',
+    didInsertElement: function() {
+        $('body').addClass('aboutView')
+    },
+    willClearRender: function() {
+        $('body').removeClass('aboutView')
+    }
+});
+
+App.CatalogView = Em.View.extend({
+    templateName: 'catalog',
+    didInsertElement: function() {
+        $('body').addClass('catalogView')
+    },
+    willClearRender: function() {
+        $('body').removeClass('catalogView')
+    }
+});
+
+App.BianchiView = Em.View.extend({
+    templateName: 'bianchi',
+    didInsertElement: function() {
+        $('body').addClass('bianchiView')
+    },
+    willClearRender: function() {
+        $('body').removeClass('bianchiView')
+    }
+});
+
+App.CannondaleView = Em.View.extend({
+    templateName: 'cannondale',
+    didInsertElement: function() {
+        $('body').addClass('cannondaleView')
+    },
+    willClearRender: function() {
+        $('body').removeClass('cannondaleView')
+    }
+});
+
+App.CenturionView = Em.View.extend({
+    templateName: 'centurion',
+    didInsertElement: function() {
+        $('body').addClass('centurionView')
+    },
+    willClearRender: function() {
+        $('body').removeClass('centurionView')
+    }
+});
+
+App.NishikiView = Em.View.extend({
+    templateName: 'nishiki',
+    didInsertElement: function() {
+        $('body').addClass('nishikiView')
+    },
+    willClearRender: function() {
+        $('body').removeClass('nishikiView')
+    }
+});
+
+App.SpecializedView = Em.View.extend({
+    templateName: 'specialized',
+    didInsertElement: function() {
+        $('body').addClass('specializedView')
+    },
+    willClearRender: function() {
+        $('body').removeClass('specializedView')
+    }
+});
+
+App.PeugeotView = Em.View.extend({
+    templateName: 'peugeot',
+    didInsertElement: function() {
+        $('body').addClass('peugeotView')
+    },
+    willClearRender: function() {
+        $('body').removeClass('peugeotView')
+    }
+});
+
+App.RaleighView = Em.View.extend({
+    templateName: 'raleigh',
+    didInsertElement: function() {
+        $('body').addClass('raleighView')
+    },
+    willClearRender: function() {
+        $('body').removeClass('raleighView')
+    }
+});
+
+App.TrekView = Em.View.extend({
+    templateName: 'trek',
+    didInsertElement: function() {
+        $('body').addClass('trekView')
+    },
+    willClearRender: function() {
+        $('body').removeClass('trekView')
+    }
+});
+
+App.JapaneseView = Em.View.extend({
+    templateName: 'japanese',
+    didInsertElement: function() {
+        $('body').addClass('japaneseView')
+    },
+    willClearRender: function() {
+        $('body').removeClass('japaneseView')
+    }
+});
+
+App.FrenchView = Em.View.extend({
+    templateName: 'french',
+    didInsertElement: function() {
+        $('body').addClass('frenchView')
+    },
+    willClearRender: function() {
+        $('body').removeClass('frenchView')
+    }
+});
+
+App.EnglishView = Em.View.extend({
+    templateName: 'english',
+    didInsertElement: function() {
+        $('body').addClass('englishView')
+    },
+    willClearRender: function() {
+        $('body').removeClass('englishView')
+    }
+});
+
+App.ItalianView = Em.View.extend({
+    templateName: 'italian',
+    didInsertElement: function() {
+        $('body').addClass('italianView')
+    },
+    willClearRender: function() {
+        $('body').removeClass('italianView')
+    }
+});
+
+App.UsaView = Em.View.extend({
+    templateName: 'usa',
+    didInsertElement: function() {
+        $('body').addClass('usaView')
+    },
+    willClearRender: function() {
+        $('body').removeClass('usaView')
+    }
+});
+
+App.VendorsView = Em.View.extend({
+    templateName: 'vendors',
+    didInsertElement: function() {
+        $('body').addClass('vendorsView')
+    },
+    willClearRender: function() {
+        $('body').removeClass('vendorsView')
+    }
+});
