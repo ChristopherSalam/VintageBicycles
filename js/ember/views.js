b0dc4d161a329145136d1c6731167cecc7e4d3d9
chrissalam <christopher.salam@gmail.com>
2015-05-14 12:56:58 -0700

    Data--> up to date with FTP and Design, set to close branches

--- js/ember/views.js
+++ js/ember/views.js
@@ -1,12 +1,14 @@
 App.IndexView = Em.View.extend({
     templateName: 'index',
     didInsertElement: function() {
-        $('body').addClass('indexView'),
-        $('header>a>img').addClass('outline')
+        $('body').addClass('indexView')
+        $('header>a>img.outlineLogo').addClass('showLogo')
+        $('header>a>img.normalLogo').addClass('hideLogo')
     },
     willClearRender: function() {
-        $('body').removeClass('indexView'),
-        $('header>a>img').removeClass('outline')
+        $('body').removeClass('indexView')
+        $('header>a>img.outlineLogo').removeClass('showLogo')
+        $('header>a>img.normalLogo').removeClass('hideLogo')
     }
 })
 
@@ -34,9 +36,13 @@ App.CatalogView = Em.View.extend({
     templateName: 'catalog',
     didInsertElement: function() {
         $('body').addClass('catalogView')
+        $('header>a>img.blackLogo').addClass('showLogo')
+        $('header>a>img.normalLogo').addClass('hideLogo')
     },
     willClearRender: function() {
         $('body').removeClass('catalogView')
+        $('header>a>img.blackLogo').removeClass('showLogo')
+        $('header>a>img.normalLogo').removeClass('hideLogo')
     }
 });
 
